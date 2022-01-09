38402e99cdabdfb8dea6d62c2974792cf0e5b539
Murali Krishnamurthy <muralimk@outlook.com>
2022-01-09 13:22:18 +0530

    Added Web and Models projects

---
+++ STVMatrimonyModels/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace STVMatrimonyModels
+{
+    public class Class1
+    {
+    }
+}
---
+++ STVMatrimonyServices/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace STVMatrimonyServices
+{
+    public class Class1
+    {
+    }
+}
