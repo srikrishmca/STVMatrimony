67613adab1c4081b78826b58b13843bd480ad035
SRIKRISHNAN <srikrishnanmca@live.com>
2022-02-08 19:12:52 +0530

    Added  Fonts and Styles

--- STVMatrimony/STVMatrimony/AssemblyInfo.cs
+++ STVMatrimony/STVMatrimony/AssemblyInfo.cs
@@ -1,3 +1,14 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("FontAwesome-Regular.ttf", Alias = "FontAwesome_Regular")]
+[assembly: ExportFont("FontAwesome-Solid.ttf", Alias = "FontAwesome_Solid")]
+[assembly: ExportFont("Mulish-Black.ttf", Alias = "Mulish_Black")]
+[assembly: ExportFont("Mulish-Bold.ttf", Alias = "Mulish_Bold")]
+[assembly: ExportFont("Mulish-ExtraBold.ttf", Alias = "Mulish_ExtraBold")]
+[assembly: ExportFont("Mulish-ExtraLight.ttf", Alias = "Mulish_ExtraLight")]
+[assembly: ExportFont("Mulish-Light.ttf", Alias = "Mulish_Light")]
+[assembly: ExportFont("Mulish-Medium.ttf", Alias = "Mulish_Medium")]
+[assembly: ExportFont("Mulish-Regular.ttf", Alias = "Mulish_Regular")]
+[assembly: ExportFont("Mulish-SemiBold.ttf", Alias = "Mulish_SemiBold")]
---
+++ STVMatrimony/STVMatrimony/Styles/Theme.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace STVMatrimony.Styles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Theme : ResourceDictionary
+    {
+        public Theme()
+        {
+            InitializeComponent();
+        }
+    }
+}
